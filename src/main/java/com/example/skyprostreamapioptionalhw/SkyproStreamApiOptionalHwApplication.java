b08a0eb49f4ec16b639eb4e8f464fbf6b8881697
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-08-01 18:43:18 +0300

    Initial commit

---
+++ src/main/java/com/example/skyprostreamapioptionalhw/SkyproStreamApiOptionalHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyprostreamapioptionalhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproStreamApiOptionalHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproStreamApiOptionalHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skyprostreamapioptionalhw/SkyproStreamApiOptionalHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprostreamapioptionalhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproStreamApiOptionalHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
